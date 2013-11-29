85d3613b91a1c737080c2577c57da9e6210910ac
BoberUnity <borisberez@rambler.ru>
2013-11-29 19:44:47 +0400

    Test Buying

---
+++ Assets/Editor/Prime31Shared/P31Unity.mm
@@ -0,0 +1,34 @@
+//
+//  P31Unity.m
+//  P31SharedTools
+//
+//  Created by Mike Desaro on 9/12/13.
+//  Copyright (c) 2013 prime31. All rights reserved.
+//
+
+#import "P31Unity.h"
+
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+	void UnitySendMessage( const char * className, const char * methodName, const char * param );
+#ifdef __cplusplus
+}
+#endif
+
+UIViewController *UnityGetGLViewController();
+void UnityPause( bool pause );
+
+
+@implementation P31Unity
+
++ (void)unityPause:(NSNumber*)shouldPause
+{
+	BOOL shouldPauseBool = [shouldPause boolValue];
+	UnityPause( shouldPauseBool );
+}
+
+
+@end
