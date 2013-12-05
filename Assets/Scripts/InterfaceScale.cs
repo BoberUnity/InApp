3e2a2320fd015521474bc1b342c585439752e674
BoberUnity <borisberez@rambler.ru>
2013-12-05 18:23:19 +0400

    RenderTexture

--- Assets/Scripts/InterfaceScale.cs
+++ Assets/Scripts/InterfaceScale.cs
@@ -1,1 +1,25 @@
-using UnityEngine;public class InterfaceScale : MonoBehaviour{    [SerializeField] private bool position = true;    [SerializeField] private bool scale = false;    private void Start()  {      if (position)          transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y * 0.5625f * Screen.height / Screen.width, transform.localPosition.z);      if (scale)        transform.localScale = new Vector3(1,0.5625f * Screen.height/Screen.width ,1);  }}
+using UnityEngine;
+
+public class InterfaceScale : MonoBehaviour
+{
+    [SerializeField] private bool position = true;
+    [SerializeField] private bool scale = false;
+    //[SerializeField] private bool scaleX = false;
+
+    private void Start()
+    {
+        float sx = transform.localScale.x;
+        float sy = transform.localScale.y;
+        float sz = transform.localScale.z;
+        if (position)
+          transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y * 0.5625f * Screen.height / Screen.width, transform.localPosition.z);
+      if (scale)
+      {
+          float scaleFactor = 0.5625f*Screen.height/Screen.width;
+          //if (scaleX)
+              transform.localScale = new Vector3(sx * scaleFactor, sy * scaleFactor, sz * scaleFactor);
+          //else
+          //    transform.localScale = new Vector3(sx, sy * scaleFactor, sz);
+      }
+  }
+}
