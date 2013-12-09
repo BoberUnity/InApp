2cdd72994f9b893c17741171e22e6db830f345e1
BoberUnity <borisberez@rambler.ru>
2013-12-09 17:44:59 +0400

    Синхронизация подложки под 3Д окно по вертикали со скроллом на различных разрешениях экрана

--- Assets/Scripts/Render3D.cs
+++ Assets/Scripts/Render3D.cs
@@ -7,6 +7,8 @@ public class Render3D : MonoBehaviour
     [SerializeField] private Transform scroll = null;
     [SerializeField] private TextChild textController = null;
     [SerializeField] private float speed = 1;
+    [SerializeField]
+    private float wh = 1;
     private bool play = false;
     private float actualAspect = 0.75f;
     private float viewHeight = 0.22f;
@@ -15,6 +17,7 @@ public class Render3D : MonoBehaviour
     private bool cam1Left = false;
 
     private float y = 0;
+    private float posY = 0;
 
     public bool Play
     {
@@ -33,12 +36,10 @@ public class Render3D : MonoBehaviour
         transform.localScale = new Vector3(16/(actualAspect *9), 16/(actualAspect *9), 1);
         viewHeight = 0.22f*1.7777f/actualAspect;
     }
-
+    
     private void Update()
     {
         y = (scroll.localPosition.y - 65) / (1150 / (1.7777f / actualAspect));//scroll.localPosition.y / (321 * actualAspect) + 0.28f;//Движение за скроллом
-
-        
         
         if (play)//Анимация
         {
@@ -70,10 +71,13 @@ public class Render3D : MonoBehaviour
                 camera1.rect = (new Rect(0.04f, y + 0.59f, 0.92f, viewHeight));
             }
         }
-
+        screen1.localPosition = new Vector3(screen1.localPosition.x, scroll.localPosition.y / 576 - 0.11f + (1.77778f - actualAspect) * 0.11f, screen1.localPosition.z);
+        screen2.localPosition = new Vector3(screen2.localPosition.x, scroll.localPosition.y / 576 - 0.11f + (1.77778f - actualAspect) * 0.11f, screen2.localPosition.z);
+        wh = 1.77778f - actualAspect;
         if (camera1 != null)
             camera1.rect = (new Rect(screen1.localPosition.x / 1.13f + 0.04f, y + 0.59f, 0.92f, viewHeight));
         if (camera2 != null)
             camera2.rect = (new Rect(screen2.localPosition.x / 1.13f + 0.04f, y + 0.59f, 0.92f, viewHeight));
+        
     }
 }
