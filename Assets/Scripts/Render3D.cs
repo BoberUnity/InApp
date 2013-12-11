03beda98f36ab27b9d986d3f89ecab57851e77c1
BoberUnity <borisberez@rambler.ru>
2013-12-11 15:26:28 +0400

    Анимация плашки под вопросом в викторине

--- Assets/Scripts/Render3D.cs
+++ Assets/Scripts/Render3D.cs
@@ -4,6 +4,8 @@ public class Render3D : MonoBehaviour
 {
     [SerializeField] private Transform screen1 = null;
     [SerializeField] private Transform screen2 = null;
+    [SerializeField] private Transform sprite1 = null;
+    [SerializeField] private Transform sprite2 = null;
     [SerializeField] private Transform scroll = null;
     [SerializeField] private TextChild textController = null;
     [SerializeField]
@@ -38,6 +40,8 @@ public class Render3D : MonoBehaviour
     private void Update()
     {
         y = (scroll.localPosition.y - 65) / (1150 / (1.7777f / actualAspect));//scroll.localPosition.y / (321 * actualAspect) + 0.28f;//Движение за скроллом
+
+        
         
         if (play)//Анимация
         {
@@ -76,6 +80,11 @@ public class Render3D : MonoBehaviour
         }
         screen1.localPosition = new Vector3(screen1.localPosition.x, scroll.localPosition.y / 576 - 0.11f + (1.77778f - actualAspect) * 0.11f, screen1.localPosition.z);
         screen2.localPosition = new Vector3(screen2.localPosition.x, scroll.localPosition.y / 576 - 0.11f + (1.77778f - actualAspect) * 0.11f, screen2.localPosition.z);
+
+        sprite1.localPosition = new Vector3(screen1.localPosition.x * 620 / 1.13f, scroll.localPosition.y - 65,
+                                                sprite1.localPosition.z);
+        sprite2.localPosition = new Vector3(screen2.localPosition.x * 620 / 1.13f, scroll.localPosition.y - 65,
+                                            sprite2.localPosition.z);
         if (camera1 != null)
             camera1.rect = (new Rect(screen1.localPosition.x / 1.13f + 0.04f, y + 0.59f, 0.92f, viewHeight));
         if (camera2 != null)
